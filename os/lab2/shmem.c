b88e82bd65912a179a65196cb5e31a61ffeffe00
AlexHff <hello.alex.buzz@gmail.com>
2019-09-22 14:53:53 +0200

    Update OS Lab2

--- os/lab2/shmem.c
+++ os/lab2/shmem.c
@@ -12,9 +12,12 @@
 int main(int argc, char **argv) {
   int id, i;
   int *ptr;
+  // Show information on IPC facilities about active shared memory segments.
   system("ipcs -m");
-  id=shmget(KEY, sizeof(int), IPC_CREAT | PERMS);
+  // Allocates a System V shared memory segment.
+  id = shmget(KEY, sizeof(int), IPC_CREAT | PERMS);
   system("ipcs -m");
+  // Shared memory operations
   ptr = (int*) shmat(id, NULL, 0);
   *ptr = 54;
   i = 54;
@@ -25,6 +28,7 @@ int main(int argc, char **argv) {
   } else {
     wait(NULL);
     printf("Value of *ptr = %d\nValue of i = %d\n", *ptr, i);
+    // System V shared memory control
     shmctl(id, IPC_RMID, NULL);
   }
 }
