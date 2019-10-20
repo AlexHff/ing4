f6cf4978865ce68c1308727dfc8a8aca4e00ead5
AlexHff <hello.alex.buzz@gmail.com>
2019-10-20 15:02:43 +0200

    OS Finish Lab4

--- os/lab4/bcall.c
+++ os/lab4/bcall.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-  int i;
-  char buf[100];
-  fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK); // Manipulates the STDIN_FILENO file descriptor 
+  int nread, i;
+  char buf[128];
+  //fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+  fcntl(STDIN_FILENO, F_SETFL, 0);
   for (i = 0; i < 10; i++) {
-    int nb;
-    nb = read(STDIN_FILENO, buf, sizeof(buf));
-    printf("nwrites = %d\terror = %d\n", nb, errno);
+    nread = read(0, buf, 128);
+    if(nread == -1)
+      perror("read");
+    else
+      printf("ok");
+    printf("nwrites = %d\terror = %d\n", nread, errno);
   }
   return 0;
 }
