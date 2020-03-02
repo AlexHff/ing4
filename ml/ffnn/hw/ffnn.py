632e3eb2a96e439353c5d72d94a28d9eb3220311
AlexHff <hello.alex.buzz@gmail.com>
2020-03-02 20:48:37 +0100

    ml algos

--- ml/ffnn/hw/ffnn.py
+++ ml/ffnn/hw/ffnn.py
@@ -1,48 +1,67 @@
+#!/usr/bin/python3
+
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-def sigmoid(x):
-  return 1.0 / (1.0 + np.exp(-x))
+class NeuralNetwork():
+  def __init__(self):
+    np.random.seed(1)
+    self.weights = {}
+    self.num_layers = 1
 
-def labels(x):
-  labels = [len(x)]
-  for i in range (0, len(x)):
-    labels[i] = max(x[i][0], 3.0)
-  return labels
+  def add_layer(self, shape):
+    self.weights[self.num_layers] = np.vstack((2 * np.random.random(shape) - 1, 2 * np.random.random((1, shape[1])) - 1))
+    self.num_layers += 1
 
-def sse(Y, y):
-  return 1.0 / 2.0 * sum((Y - y)**2)
+  def sigmoid(self, x):
+    return 1.0 / (1.0 + np.exp(-x))
 
-# Load data from file
-fic = open("data_ffnn_3classes.txt", "r")
-data = np.loadtxt(fic)
-fic.close()
+  def predict(self, data):
+    for layer in range(1, self.num_layers+1):
+      data = np.dot(data, self.weights[layer-1][:, :-1]) + self.weights[layer-1][:, -1]
+      data = self.sigmoid(data)
+    return data
 
-# Input data matrix
-x = np.array([np.ones(len(data)), data[:, 0], data[:, 1]]).transpose()
+  def forward_propagate(self, data):
+    for layer in range(2, self.num_layers+1):
+      data = np.dot(data.T, self.weights[layer-1][:-1, :]) + self.weights[layer-1][-1, :].T
+      data = self.sigmoid(data).T
+    return data
 
-# Actual output
-y = data[:, 2]
+if __name__ == '__main__':
+  # Load data from file
+  fic = open("data_ffnn_3classes.txt", "r")
+  data = np.loadtxt(fic)
+  fic.close()
 
-# Initialize parameters
-v = np.random.rand(3,3)
+  # Input data matrix
+  X = np.array([np.ones(len(data)), data[:, 0], data[:, 1]]).transpose()
+  plt.scatter(X[:,1],X[:,2])
 
-# Multiply matrices
-X = x.dot(v)
+  # Actual output
+  Y = np.array(data[:,2])
 
-# Apply activation function
-f = sigmoid(X)
+  plt.show()
+  """
+  # Initialize parameters
+  v = np.random.rand(3,3)
 
-# Add columns of ones
-F = np.array([np.ones(len(f)), f[:, 0], f[:, 1], f[:, 2]]).transpose()
+  # Multiply matrices
+  X = x.dot(v)
 
-# Init params
-w = np.random.rand(4,3)
+  # Apply activation function
+  f = sigmoid(X)
 
-# Multiply matrices
-FF = F.dot(w)
+  # Add columns of ones
+  F = np.array([np.ones(len(f)), f[:, 0], f[:, 1], f[:, 2]]).transpose()
 
-# Apply activation function
-G = sigmoid(FF)
-print(G)
+  # Init params
+  w = np.random.rand(4,3)
+
+  # Multiply matrices
+  FF = F.dot(w)
+
+  # Apply activation function
+  G = sigmoid(FF)
+  """
